3a05d3a5ec525ba0d238f2994dfa9c5efa7bd407
legoandmars <legoandmars@gmail.com>
2024-02-16 19:19:12 -0800

    Remove unused property

--- Runtime/LCPatcherShaderInjectionSettings.cs
+++ Runtime/LCPatcherShaderInjectionSettings.cs
@@ -5,7 +5,6 @@ namespace Nomnom.LCProjectPatcher {
     // note: needs to be in Resources to be picked up in runtime builds
     [CreateAssetMenu(fileName = "ShaderInjectionSettings", menuName = "LC Project Patcher/Shader Injection Settings")]
     public class LCPatcherShaderInjectionSettings: ScriptableObject {
-        public bool EnableShaderInjections = true;
         public List<ShaderInjection> ShaderInjections = new();
 
         public void InjectAllShadersIntoMaterials() {
